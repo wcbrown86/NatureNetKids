e6d25441a8ecb1e6cb7e065d226b8366066134b9
cbryan31 <cbryan31@uncc.edu>
2017-04-12 18:22:17 -0400

    Added signup and logout features

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,34 +4,42 @@ import { NavController , ModalController} from 'ionic-angular';
 import { TakePhotoPage } from '../take-photo/take-photo';
 import { ObservationsPage } from '../observations/observations';
 import { LoginPage } from '../login/login';
+import { SignupPage } from '../signup/signup';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  loginPage = LoginPage;  //Added login page for logging out
  
   constructor(public navCtrl: NavController,
     public modalCtrl: ModalController) {}
   
-takePhotoPage(){
+    takePhotoPage(){
 
-  let photoModal = this.modalCtrl.create(TakePhotoPage);
-  photoModal.present();
+      let photoModal = this.modalCtrl.create(TakePhotoPage);
+      photoModal.present();
 
-}
+    }
 
-seeProjects(){
+    seeProjects(){
 
-  let observationsModal = this.modalCtrl.create(ObservationsPage);
-  observationsModal.present();
+      let observationsModal = this.modalCtrl.create(ObservationsPage);
+      observationsModal.present();
 
-} 
+    } 
 
-goToLogin(){
+    goToLogin(){
 
-  let loginModal = this.modalCtrl.create(LoginPage);
-  loginModal.present();
-}
+      let loginModal = this.modalCtrl.create(LoginPage);
+      loginModal.present();
+    }
+
+    goToSignup(){
+
+      let signupModal = this.modalCtrl.create(SignupPage);
+      signupModal.present();
+    }
 
 }
