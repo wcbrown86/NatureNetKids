b1bd15e59622eb52bd24f056de74dfe3ed227c81
cbryan31 <cbryan31@uncc.edu>
2017-04-25 18:28:30 -0400

    Added a observation detail page

---
+++ src/pages/detail-observation/detail-observation.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
+
+import { HomePage } from '../home/home';
+
+/*
+  Generated class for the DetailObservation page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-detail-observation',
+  templateUrl: 'detail-observation.html'
+})
+export class DetailObservationPage {
+
+  observation: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+    this.observation = navParams.get('observation');
+  }
+
+   home(){
+    let goHome = this.modalCtrl.create(HomePage);
+    goHome.present();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailObservationPage');
+  }
+
+}
