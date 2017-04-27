b234dabe48632ca5d372b96c70f48f7c7d871a75
wbrown54 <wbrown54@uncc.edu>
2017-04-27 00:40:13 -0400

    Started Commiting all pages for submission.

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,26 +1,40 @@
+
+//Default import for Ionic.
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+//Imports needed for navigation controls.
+import { NavController, NavParams, ModalController } from 'ionic-angular';
 
-/*
-  Generated class for the Help page.
+//Page Imports needed to navigate to different pages. 
+import { HomePage } from '../home/home';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-help',
   templateUrl: 'help.html'
 })
 export class HelpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  //Constructor that generates variables needed for navigation.
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+        public modalCtrl: ModalController) {}
 
+  //Page load log function, not needed for the app to function
+  //left for error detection if needed.
   ionViewDidLoad() {
     console.log('ionViewDidLoad HelpPage');
   }
 
+  //Close function that dimisses the current page by popping it off the stack.
   close(){
+    //pops the current page off the stack takeing the user back to the previous page.
     this.navCtrl.pop();
+  }
+
+  //Home function that sends the user back to the home page
+  home(){
+
+    //Creates the modal and presents the page.
+    let homeModel = this.modalCtrl.create(HomePage);
+    homeModel.present();
   }
 
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,41 +1,54 @@
+
+//Default import from Ionic page generation 
 import { Component } from '@angular/core';
+//Import needed for navigation controls.
 import { NavController, ModalController, NavParams } from 'ionic-angular';
-//added AuthProvider
+//added AuthProvider, needed for logout function
 import { AuthProvider } from '../../providers/auth-provider';
+//Page imports needed for navigation
 import { LoginPage } from '../login/login';
+import { HomePage } from '../home/home';
 
-/*
-  Generated class for the Settings page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
 })
+
 export class SettingsPage {
 
+  //Constructor that create variables need for navigation and AuthProvider
+  //all variables are injected with the constructor. 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public modalCtrl: ModalController,
     public auth: AuthProvider) {}
 
+    //Page load log function, not needed for the app to function
+  //left for error detection if needed.
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
 
+  //login function that take the user to the login and register page.
   goToLogin() {
 
+    //Creates the modal view and presents the modal.
     let loginModal = this.modalCtrl.create(LoginPage);
     loginModal.present();
   }
 
+  //function that is called to take the user back to the home page.
+  //Settings page is only accessable from the home page. the home
+  //button calls this same function to pop the settings page off the stack
   close() {
+
+    //pops the settings page off the stack.
     this.navCtrl.pop();
   }
 
+  //this function is called to log the user out
   logout(): void {
 
+    //uses the authProvider logout funciton to log the user out.
     this.auth.logout();
   }
 
