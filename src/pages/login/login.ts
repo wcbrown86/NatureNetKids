05095c4e62340374f1dee3e722c32a47e5dd2591
cbryan31 <cbryan31@uncc.edu>
2017-04-29 17:23:08 -0400

    Fixed login for error handling

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -42,42 +42,25 @@ export class LoginPage {
 
   login(): void { 
     if(this.loginForm.valid) {
-        //console.log(this.email.value, this.password.value);
-        alert('Implement authentication '+this.email.value+", "+this.password.value);
         var credentials = ({email: this.email.value, password: this.password.value}); //Added next lines
           this.auth.loginWithEmail(credentials).subscribe(data => { 
-            console.log(data);
              //Displays in browser message for login succussful and then goes to home page
              if (data)
-            {
-              alert('Login Successful!!!');
-				
-			  let homeModal = this.modalCtrl.create(HomePage);
-  			  homeModal.present();
-
-            }
+              {
+                alert('Login Successful!!!');
+                this.navCtrl.setRoot(HomePage);
+               // let homeModal = this.modalCtrl.create(HomePage);
+               // homeModal.present();
+              }
             }, error=>{             //Added next lines for handling unknown users
-            console.log(error);
-            if (error.code == 'auth/user-not-found')
-            {
-              alert('User not found');
-            }
-
+            if (error.code == 'auth/user-not-found'){ alert('User Not Found'); }//user not found
+            if (error.code == 'auth/wrong-password'){ alert('Incorrect Password'); }//incorrect password
           });
+    }else{
+      alert("You must fillout all the fields correctly to login.");
     }
 
 }
-
-  //logout funciton that uses AuthProvider function to log the user out
-  logout(): void {
-
-    //Function call to log the user out
-    this.auth.logout();
-
-    //creates the model for the home page and presents the page.
-    let homeModal = this.modalCtrl.create(HomePage);
-    homeModal.present();
-  }
 
   //when called takes the user to the signup page
   goToSignup(){
@@ -90,9 +73,10 @@ export class LoginPage {
   //When called function takes the user to the home page 
   home(){
 
-    //crates the modal and presents the page 
-    let homeModal = this.modalCtrl.create(HomePage);
-    homeModal.present();
+    //crates the modal and presents the page
+    this.navCtrl.setRoot(HomePage);
+    //let homeModal = this.modalCtrl.create(HomePage);
+    //homeModal.present();
   }
 
   //Default function that logs the page load information 
