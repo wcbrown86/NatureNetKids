8b1de2760d0bc7d680091d6728805d2854e214c0
cbryan31 <cbryan31@uncc.edu>
2017-04-29 18:08:23 -0400

    No major changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 
+
 @Component({
-  templateUrl: 'app.html'
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
   rootPage = HomePage;
@@ -14,8 +15,9 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+        StatusBar.styleDefault();
+        Splashscreen.hide();
     });
   }
+
 }
