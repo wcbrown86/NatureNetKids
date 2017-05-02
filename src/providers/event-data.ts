3650b267ac64ca392cec968798f4434b8f88b15e
cbryan31 <cbryan31@uncc.edu>
2017-05-02 19:34:34 -0400

    Added a return line which retrieves only the NatureNetKids project observations

--- src/providers/event-data.ts
+++ src/providers/event-data.ts
@@ -4,23 +4,18 @@ import 'rxjs/add/operator/map';
 
 import * as firebase from 'firebase';
 
-/*
-  Generated class for the EventData provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class EventData {
 
   constructor() {
-    //console.log('Hello EventData Provider');
   }
 
   getLimitedCategoryList(number): any {
-    return firebase.database().ref('/observations').limitToLast(number);
+    //return firebase.database().ref('/observations').limitToLast(number);//gets all observations
+    return firebase.database().ref('/observations').orderByChild("activity").equalTo('NNK').limitToLast(number);//get only the NNK observations
   }
 
+//Not implemented, is used to get the users form firebase for name retrieval
   getUserFromFirebase(uid){
     return firebase.database().ref('/users').equalTo(uid);
   }
